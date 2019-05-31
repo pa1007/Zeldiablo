c4c88aee75279f17eaa7edc60f20a3d5afa3bd82
paullouppe <paullouppe@gmail.com>
2019-05-31 10:16:28 +0200

    se deplacer

--- projet_zeldiablo/Personnage.java
+++ projet_zeldiablo/Personnage.java
@@ -1,3 +1,5 @@
+import utils.Place;
+
 public class Personnage {
 
     private String nom;
@@ -7,6 +9,26 @@ public class Personnage {
     public Personnage(String nom, Place position){
         this.nom = nom;
         this.position = position;
+    }
+
+    public void seDeplacer(char direction){
+        switch (direction){
+            case 'N':
+                this.position.incrementerY(1);
+                break;
+            case 'S':
+                this.position.incrementerY(-1);
+                break;
+            case 'E':
+                this.position.incrementerX(1);
+                break;
+            case 'O':
+                this.position.incrementerX(-1);
+                break;
+                default:
+                    System.out.println("Caractere non valide");
+                    break;
+        }
     }
 
 
