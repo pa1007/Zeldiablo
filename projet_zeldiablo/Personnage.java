58174ba7cb2826e88315785d147d3a93837df367
paullouppe <paullouppe@gmail.com>
2019-05-31 10:10:19 +0200

    Personnage de base

---
+++ projet_zeldiablo/Personnage.java
@@ -0,0 +1,13 @@
+public class Personnage {
+
+    private String nom;
+    private Place position;
+
+
+    public Personnage(String nom, Place position){
+        this.nom = nom;
+        this.position = position;
+    }
+
+
+}
